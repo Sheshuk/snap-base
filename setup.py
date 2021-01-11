a4a49decb5ca3694096f234c527d1f6881db6f09
Andrey Sheshukov <ash@jinr.ru>
2021-01-11 23:43:32 +0300

    Setuptools instead of disttools

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools 
+
 with open('README.md') as f:
     readme = f.read()
 
-setup(name='snap-base',
+setuptools.setup(name='snap-base',
         version='1.0.0',
         description='SuperNova Async Pipeline: base package',
         long_description=readme,
@@ -15,6 +16,7 @@ setup(name='snap-base',
         licence='GNU GPLv3',
         packages=['snap'],
         scripts=['scripts/snap'],
-        install_requires=['pyzmq>=20','pyyaml>=3.5']
-     )
+        install_requires=['pyzmq>=20','pyyaml>=3.5'],
+        python_requires='>=3.6'
+        )
 
