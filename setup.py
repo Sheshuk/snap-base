a12748143fb22165bb48c344fcb07f9d96a2aca8
Andrey Sheshukov <ash@jinr.ru>
2021-05-26 16:22:25 +0300

    Moved all utils into a submodule

--- setup.py
+++ setup.py
@@ -19,9 +19,13 @@ setuptools.setup(name='snap-base',
         author='Andrey Sheshukov',
         author_email='ash@jinr.ru',
         licence='GNU GPLv3',
-        packages=['snap'],
+        packages=['snap', 'snap.io', 'snap.util'],
         scripts=['scripts/snap'],
-        install_requires=['pyyaml>=3.5'],
+        install_requires=[
+            'pyyaml>=3.5', 
+            'tqdm>=4.53',
+            'numpy',
+            ],
         extras_require=dict(
             doc=['sphinx', 'sphinx-rtd-theme']+extras['io'],
             **extras
---
+++ snap/util/__init__.py
@@ -0,0 +1,8 @@
+"""
+Utility modules
+===============
+"""
+from .tqdm import *
+from .misc import *
+from .timing import *
+from .output import *
